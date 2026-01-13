b931d7c0a5c124f0733528356af314c866199efd
kova <kovds12@gmail.com>
2026-01-13 21:57:12 +0300

    fix(core): update server config

--- internal/delivery/http/server.go
+++ internal/delivery/http/server.go
@@ -3,13 +3,33 @@ package http
 import (
 	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
+	"time"
 
-	"github.com/Kovalyovv/auth-service/internal/config"
+	conf "github.com/Kovalyovv/auth-service/internal/config"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
-func NewServer(cfg *config.Config, handler *AuthHandler) *http.Server {
+func NewServer(cfg *conf.Config, handler *AuthHandler) *http.Server {
 	router := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowCredentials = true
+	config.AllowOriginFunc = func(origin string) bool {
+		u, err := url.Parse(origin)
+		if err != nil {
+			return false
+		}
+		hostname := strings.Trim(u.Hostname(), "[]")
+		return (hostname == "localhost" || hostname == "0.0.0.0" || hostname == "127.0.0.1" || hostname == "::1") && (u.Port() == "9000" || u.Port() == "9002" || u.Port() == "9001")
+	}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
+	config.MaxAge = 12 * time.Hour
+
+	router.Use(cors.New(config))
 
 	auth := router.Group("/auth")
 	{
